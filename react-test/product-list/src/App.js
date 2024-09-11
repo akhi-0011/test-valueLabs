c8fe2d0d355edc401c3ccb0a8a0e39ae38d78456
akhi-0011 <akhi.singh98877@gmail.com>
2024-09-11 14:27:28 +0530

    initial commit

---
+++ react-test/product-list/src/App.js
@@ -0,0 +1,21 @@
+import ProductTable from './ProductTable';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductTable />
+    </div>
+  );
+}
+
+export default App;
+
+// React js -
+
+ 
+
+// Use the following API and show the Product Name, Description and Image in tabular format. Use CSS to make it visually appealing.
+
+ 
+
+// https://dummyjson.com/products
